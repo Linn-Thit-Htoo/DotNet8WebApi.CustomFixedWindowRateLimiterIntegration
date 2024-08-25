bb655fa81c15a50cad4686cec8916183a9c6b64f
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-25 11:07:21 +0630

    Add project files.

---
+++ DotNet8WebApi.CustomFixedWindowRateLimiterIntegration/Controllers/BlogController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace DotNet8WebApi.CustomFixedWindowRateLimiterIntegration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public BlogController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBlogs()
+        {
+            HttpResponseMessage response = await _httpClient.PostAsync("/api/RateLimiting/fixed-window", null);
+            var responseJson = await response.Content.ReadAsStringAsync();
+            var statusCode = response.StatusCode;
+
+            if (statusCode == HttpStatusCode.TooManyRequests)
+            {
+                return StatusCode(429, responseJson);
+            }
+
+            return Ok();
+        }
+    }
+}
