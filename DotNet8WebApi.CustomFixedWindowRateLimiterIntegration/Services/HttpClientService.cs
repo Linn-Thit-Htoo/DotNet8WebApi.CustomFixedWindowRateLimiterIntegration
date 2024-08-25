e5843f483e59329ad6191e9fc9656eeb708abc21
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-25 13:34:44 +0630

    -

--- DotNet8WebApi.CustomFixedWindowRateLimiterIntegration/Services/HttpClientService.cs
+++ DotNet8WebApi.CustomFixedWindowRateLimiterIntegration/Services/HttpClientService.cs
@@ -1,49 +1,52 @@
-﻿namespace DotNet8WebApi.CustomFixedWindowRateLimiterIntegration.Services
+﻿namespace DotNet8WebApi.CustomFixedWindowRateLimiterIntegration.Services;
+
+public class HttpClientService
 {
-    public class HttpClientService
+    private readonly HttpClient _httpClient;
+
+    public HttpClientService(HttpClient httpClient)
     {
-        private readonly HttpClient _httpClient;
+        _httpClient = httpClient;
+    }
 
-        public HttpClientService(HttpClient httpClient)
+    public async Task<T> ExecuteAsync<T>(
+        string endpoint,
+        EnumStatusCode statusCode,
+        object? requestModel = null
+    )
+    {
+        HttpResponseMessage? response = null;
+        HttpContent? content = null;
+
+        if (requestModel is not null)
         {
-            _httpClient = httpClient;
+            string jsonStr = JsonConvert.SerializeObject(requestModel);
+            content = new StringContent(jsonStr);
         }
 
-        public async Task<T> ExecuteAsync<T>(string endpoint, EnumStatusCode statusCode, object? requestModel = null)
+        switch (statusCode)
         {
-            HttpResponseMessage? response = null;
-            HttpContent? content = null;
-
-            if (requestModel is not null)
-            {
-                string jsonStr = JsonConvert.SerializeObject(requestModel);
-                content = new StringContent(jsonStr);
-            }
-
-            switch (statusCode)
-            {
-                case EnumStatusCode.GET:
-                    response = await _httpClient.GetAsync(endpoint);
-                    break;
-                case EnumStatusCode.POST:
-                    response = await _httpClient.PostAsync(endpoint, content);
-                    break;
-                case EnumStatusCode.PUT:
-                    response = await _httpClient.PutAsync(endpoint, content);
-                    break;
-                case EnumStatusCode.PATCH:
-                    response = await _httpClient.PatchAsync(endpoint, content);
-                    break;
-                case EnumStatusCode.DELETE:
-                    response = await _httpClient.DeleteAsync(endpoint);
-                    break;
-                case EnumStatusCode.None:
-                default:
-                    break;
-            }
-
-            var responseJson = await response!.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<T>(responseJson)!;
+            case EnumStatusCode.GET:
+                response = await _httpClient.GetAsync(endpoint);
+                break;
+            case EnumStatusCode.POST:
+                response = await _httpClient.PostAsync(endpoint, content);
+                break;
+            case EnumStatusCode.PUT:
+                response = await _httpClient.PutAsync(endpoint, content);
+                break;
+            case EnumStatusCode.PATCH:
+                response = await _httpClient.PatchAsync(endpoint, content);
+                break;
+            case EnumStatusCode.DELETE:
+                response = await _httpClient.DeleteAsync(endpoint);
+                break;
+            case EnumStatusCode.None:
+            default:
+                break;
         }
+
+        var responseJson = await response!.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<T>(responseJson)!;
     }
 }
