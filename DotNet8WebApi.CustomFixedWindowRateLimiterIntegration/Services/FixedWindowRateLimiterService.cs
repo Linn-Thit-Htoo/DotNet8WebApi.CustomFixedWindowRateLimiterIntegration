bcf35a990a630021d8cc4d5441b36426c0d56571
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-25 13:34:07 +0630

    -

--- DotNet8WebApi.CustomFixedWindowRateLimiterIntegration/Services/FixedWindowRateLimiterService.cs
+++ DotNet8WebApi.CustomFixedWindowRateLimiterIntegration/Services/FixedWindowRateLimiterService.cs
@@ -1,18 +1,20 @@
-﻿namespace DotNet8WebApi.CustomFixedWindowRateLimiterIntegration.Services
+﻿namespace DotNet8WebApi.CustomFixedWindowRateLimiterIntegration.Services;
+
+public class FixedWindowRateLimiterService
 {
-    public class FixedWindowRateLimiterService
+    private readonly HttpClient _httpClient;
+
+    public FixedWindowRateLimiterService(HttpClient httpClient)
     {
-        private readonly HttpClient _httpClient;
+        _httpClient = httpClient;
+    }
 
-        public FixedWindowRateLimiterService(HttpClient httpClient)
-        {
-            _httpClient = httpClient;
-        }
-
-        public async Task<HttpResponseMessage> ApplyFixedWindowRateLimiterAsync()
-        {
-            HttpResponseMessage response = await _httpClient.PostAsync("/api/RateLimiting/fixed-window", null);
-            return response;
-        }
+    public async Task<HttpResponseMessage> ApplyFixedWindowRateLimiterAsync()
+    {
+        HttpResponseMessage response = await _httpClient.PostAsync(
+            "/api/RateLimiting/fixed-window",
+            null
+        );
+        return response;
     }
 }
